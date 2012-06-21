0d1fe0d2634dca666c225b5997d541757f612203
Sho Hashimoto <hashimoto@shokai.org>
2012-06-22 03:10:06 +0900

    cache 12 hours

--- helpers/icon.rb
+++ helpers/icon.rb
@@ -1,5 +1,5 @@
 
 def icon(user, size=:normal)
   key = "icon_#{user}_#{size}"
-  Cache.get(key) || Cache.set(key, Icon::get(user, size), 60*60*4)
+  Cache.get(key) || Cache.set(key, Icon::get(user, size), 60*60*12)
 end
