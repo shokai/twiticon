e77a6e4338b519d185a95bb8a2bf0ca4d63ab4ee
Sho Hashimoto <hashimoto@shokai.org>
2012-06-22 03:35:48 +0900

    return 404 if user profile icon not exists

--- helpers/icon.rb
+++ helpers/icon.rb
@@ -1,5 +1,13 @@
 
 def icon(user, size=:normal)
   key = "icon_#{user}_#{size}"
-  Cache.get(key) || Cache.set(key, Icon::get(user, size), 60*60*12)
+  begin
+    return Cache.get(key) || Cache.set(key, Icon::get(user, size), 60*60*12)
+  rescue Twitter::Error::NotFound => e
+    STDERR.puts e
+    throw :halt, [404, 'not found']
+  rescue => e
+    STDERR.puts e
+    throw :halt, [500, 'Internal Server Error']
+  end
 end
