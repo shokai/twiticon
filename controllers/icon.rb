72f903c8843965d3e4c36f9d868c13a14ad276e5
Sho Hashimoto <hashimoto@shokai.org>
2012-06-21 23:15:15 +0900

    add Icon URL API

--- controllers/icon.rb
+++ controllers/icon.rb
@@ -8,3 +8,26 @@ get /^\/([a-zA-Z0-9_]+)\/(#{Icon::sizes.join('|')})$/ do
   size = params[:captures].shift.to_sym
   redirect icon user, size
 end
+
+get /^\/([a-zA-Z0-9_]+)\.(txt|json)$/ do
+  user = params[:captures].shift
+  ext = params[:captures].shift
+  case ext
+  when 'txt'
+    icon user
+  when 'json'
+    {:user => user, :icon => icon(user), :size => :normal}.to_json
+  end
+end
+
+get /^\/([a-zA-Z0-9_]+)\/(#{Icon::sizes.join('|')})\.(txt|json)$/ do
+  user = params[:captures].shift
+  size = params[:captures].shift.to_sym
+  ext = params[:captures].shift
+  case ext
+  when 'txt'
+    icon user, size
+  when 'json'
+    {:user => user, :icon => icon(user,size), :size => size}.to_json
+  end
+end
--- controllers/main.rb
+++ controllers/main.rb
@@ -6,6 +6,10 @@ before '/*.json' do
   content_type 'application/json'
 end
 
+before '/*.txt' do
+  content_type 'text/plain'
+end
+
 get '/' do
   haml :index
 end
